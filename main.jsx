46449ac4ebab266b7b04bea9ce00b8c9930f08ba
carloabascalc <carloabascalc@gmail.com>
2026-03-09 03:29:10 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ScriptFactory from './ScriptFactory'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ScriptFactory />
-  </React.StrictMode>
-)
